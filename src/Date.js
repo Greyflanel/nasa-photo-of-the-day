ed3121d3c9d4810e9a2f8f50adaa32673b40dea4
Greyflanel <richardlovelace811@gmail.com>
2019-10-09 20:09:44 -0500

    created new component to hold date data, for future use.

---
+++ src/Date.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Date = props => {
+  return (
+    <div>
+      <h3>{props.newDate}</h3>
+    </div>
+  );
+};
+
+export default Date;
--- src/PicCard.js
+++ src/PicCard.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Date from "./Date";
 
 const PetCard = props => {
   return (
     <div className="Card">
       <h1>{props.title}</h1>
-      <h3>{props.date}</h3>
+      <Date newDate={props.date}/>
       <img src={props.imgUrl} alt="Space" />
       <p>{props.details}</p>
       
